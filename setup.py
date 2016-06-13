03856c591b16456db6ed86bc5b377a1ee0e076e5
Ben Firshman <ben@firshman.co.uk>
2016-06-13 09:56:41 -0700

    Bump 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dockerrun',
-      version='0.1.0',
+      version='0.1.1',
       description='A dead simple Python library for running Docker commands',
       author='Ben Firshman',
       author_email='ben@firshman.co.uk',
